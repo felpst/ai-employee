ba6295423896b54c13a6bcaf0ffdedcf013a84a9
felpst <felpst@users.noreply.github.com>
2023-10-03 15:43:13 -0300

    refactor: move chat and core to server

--- apps/server/src/apps/core/jest.config.ts
+++ apps/server/src/apps/core/jest.config.ts
@@ -1,11 +1,11 @@
 /* eslint-disable */
 export default {
-  displayName: 'chat',
-  preset: '../../jest.preset.js',
+  displayName: 'core',
+  preset: '../../../../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/apps/chat',
+  coverageDirectory: '../../coverage/apps/core',
 };
