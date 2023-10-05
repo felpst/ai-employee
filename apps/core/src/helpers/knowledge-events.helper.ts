312492db51e34bb88ae049e2b334c1011b649d85
felpst <felpst@users.noreply.github.com>
2023-10-05 16:53:29 -0300

    refactor: use new lib to manage in knowledge hook events

--- apps/core/src/helpers/knowledge-events.helper.ts
+++ apps/core/src/helpers/knowledge-events.helper.ts
@@ -1,63 +1,26 @@
 import { IKnowledge } from '@cognum/interfaces';
+import MongoVectorDatabase, { splitDocuments } from '@cognum/mongo-vector-db';
 import { EventEmitter } from 'events';
-import { Document } from "langchain/document";
-import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
-import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
-import { MongoDBAtlasVectorSearch } from 'langchain/vectorstores/mongodb_atlas';
-import { MongoClient, ObjectId } from 'mongodb';
 
 const knowledgeOperationEventEmitter = new EventEmitter();
-
-const mongoCollection = new MongoClient(process.env.MONGO_URL).db('test').collection('nomeprovisorio')
-const mongoWithEmbeddings = new MongoDBAtlasVectorSearch(
-  new OpenAIEmbeddings({ openAIApiKey: process.env.OPENAI_API_KEY, stripNewLines: false }),
-  {
-    collection: mongoCollection,
-    textKey: 'data'
-  }
-)
+const vectorDb = new MongoVectorDatabase('nomeprovisorio');
 
 knowledgeOperationEventEmitter.on('create', async (docs: IKnowledge[]) => {
-  const splitted = await splitDocuments(docs)
-  await saveDocumentParts(splitted)
+  if (!docs) return;
+  const splitted = await splitDocuments(docs);
+  await vectorDb.addDocuments(splitted);
 });
 
 knowledgeOperationEventEmitter.on('update', async (doc: IKnowledge) => {
-  await deleteDocumentParts(doc._id)
-  const splitted = await splitDocuments([doc])
-  await saveDocumentParts(splitted)
+  if (!doc) return;
+  await vectorDb.deleteDocumentsByOwnerDocumentId(doc._id);
+  const splitted = await splitDocuments([doc]);
+  await vectorDb.addDocuments(splitted);
 });
 
 knowledgeOperationEventEmitter.on('delete', async (doc: IKnowledge) => {
-  await deleteDocumentParts(doc._id)
+  if (!doc) return;
+  await vectorDb.deleteDocumentsByOwnerDocumentId(doc._id);
 });
 
-
-async function splitDocuments(knowledgeDocs: IKnowledge[]): Promise<Document[]> {
-  const splitter = new RecursiveCharacterTextSplitter({
-    chunkSize: 1000,
-    chunkOverlap: 100,
-  });
-
-  const documents = knowledgeDocs.map(doc => 
-    new Document({ 
-      pageContent: doc.data,
-      metadata: {
-        workspace: doc.workspace,
-        documentId: doc._id,
-        updatedAt: doc.updatedAt
-      }
-    }))
-  
-  return splitter.splitDocuments(documents);
-}
-
-async function saveDocumentParts(docs: Document[]) {
-  await mongoWithEmbeddings.addDocuments(docs)
-}
-
-async function deleteDocumentParts(ownerDocumentId: ObjectId) {
-  await mongoCollection.deleteMany({ documentId: ownerDocumentId })
-}
-
-export default knowledgeOperationEventEmitter
+export default knowledgeOperationEventEmitter;
