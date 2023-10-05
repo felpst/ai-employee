93d98539067eff8dbc4d49cd8d43d42dac2b8564
felpst <felpst@users.noreply.github.com>
2023-10-05 11:31:17 -0300

    feat: middleware for knowledge event hook

--- apps/core/src/modules/knowledge/knowledge.routes.ts
+++ apps/core/src/modules/knowledge/knowledge.routes.ts
@@ -1,10 +1,10 @@
 import express, { Router } from 'express';
+import knowledgeEventEmitterHandler from '../../middlewares/knowledge-event-emitter.handler';
 import { authMiddleware } from '../auth/auth.middleware';
 import knowledgeController from './knowledge.controller';
 
 const router: Router = express.Router();
 
-router.post('/', authMiddleware, knowledgeController.create);
 router.get('/', authMiddleware, knowledgeController.find);
 router.get(
   '/workspaces/:workspaceId',
@@ -12,6 +12,10 @@ router.get(
   knowledgeController.getAllFromWorkspace
 );
 router.get('/:id', authMiddleware, knowledgeController.getById);
+
+router.use('*', knowledgeEventEmitterHandler) // all routes from here will use this handler
+
+router.post('/', authMiddleware, knowledgeController.create);
 router.put('/:id', authMiddleware, knowledgeController.update);
 router.delete('/:id', authMiddleware, knowledgeController.delete);
 
