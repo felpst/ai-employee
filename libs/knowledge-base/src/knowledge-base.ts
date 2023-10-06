ab4552c322322d5f3bf22780183ed299e410dcee
felpst <felpst@users.noreply.github.com>
2023-10-06 17:19:29 -0300

    feat: add types to knowledge base main class

--- libs/knowledge-base/src/knowledge-base.ts
+++ libs/knowledge-base/src/knowledge-base.ts
@@ -1,12 +1,17 @@
-import { Pinecone } from '@pinecone-database/pinecone';
+import { Index, Pinecone, RecordMetadata } from '@pinecone-database/pinecone';
 import { VectorDBQAChain } from 'langchain/chains';
 import { Document } from 'langchain/document';
 import { OpenAIEmbeddings } from 'langchain/embeddings/openai';
 import { OpenAI } from 'langchain/llms/openai';
 import { PineconeStore } from 'langchain/vectorstores/pinecone';
 
+interface QueryOutput {
+  text: string;
+  sourceDocuments: Document[];
+}
+
 export default class KnowledgeBase {
-  private pineconeIndex;
+  private pineconeIndex: Index<RecordMetadata>;
   private llm: OpenAI;
   private vectorStore: PineconeStore;
 
@@ -25,20 +30,22 @@ export default class KnowledgeBase {
     });
   }
 
-  async indexDocuments(docs: Document[]) {
+  async indexDocuments(docs: Document[]): Promise<string[]> {
     return this.vectorStore.addDocuments(docs);
   }
 
-  async query(input: string, documentsCount = 3) {
+  async query(input: string, documentsCount = 3): Promise<QueryOutput> {
     const chain = VectorDBQAChain.fromLLM(this.llm, this.vectorStore, {
       k: documentsCount,
       returnSourceDocuments: true,
     });
 
-    return chain.call({ query: input });
+    return chain.call({ query: input }) as Promise<QueryOutput>;
   }
 
-  async deleteDocumentsByOwnerDocumentId(ownerDocumentId: string) {
+  async deleteDocumentsByOwnerDocumentId(
+    ownerDocumentId: string
+  ): Promise<void> {
     return this.pineconeIndex.deleteMany({ ownerDocumentId });
   }
 }
