903f57ec5aade3b364e6517d04999d0cda97c403
felpst <felpst@users.noreply.github.com>
2023-10-18 00:26:20 -0300

    api integration

--- apps/app/src/app/app-routing.module.ts
+++ apps/app/src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth/auth.guard';
 import { AdminComponent } from './layouts/admin/admin.component';
+import { CreateWorkspaceComponent } from './workspaces/create-workspace/create-workspace.component';
 
 const routes: Routes = [
   // Public routes
@@ -25,6 +26,14 @@ const routes: Routes = [
         loadChildren: () =>
           import('./settings/settings.module').then(
             (m) => m.SettingsComponentModule
+          ),
+      },
+      {
+        path: 'create-workspace',
+        component: CreateWorkspaceComponent,
+        loadChildren: () =>
+          import('./workspaces/create-workspace/create-workspace.module').then(
+            (m) => m.CreateWorkspaceModule
           ),
       },
       {
