1a173c27b0b6e4a9bc4248be7cc38bf0334d031f
felpst <felpst@users.noreply.github.com>
2023-10-18 19:01:16 -0300

    feat: Added support for .svg files and improved workspace creation

--- apps/server/src/modules/workspace/workspace.schemas.ts
+++ apps/server/src/modules/workspace/workspace.schemas.ts
@@ -1,25 +1,26 @@
-  import * as yup from 'yup';
+import * as yup from 'yup';
 
-  export const addWorkspace = yup.object({
-    body: yup
-      .object({
-        name: yup.string().required(),
-        description: yup.string().notRequired().defined(),
-        workspacePhoto: yup.string().notRequired(),
-        accessLink: yup.string().notRequired(),
-        private: yup.bool().notRequired(),
-        users: yup.array().of(yup.string()).notRequired().defined(),
-        employee: yup
-          .object({
-            name: yup.string().required(),
-            role: yup.string().required(),
-            avatar: yup.string().notRequired(),
-          })
-          .notRequired()
-          .nullable(),
-      })
-      .noUnknown()
-      .required(),
-  });
+export const addWorkspace = yup.object({
+  body: yup
+    .object({
+      name: yup.string().required(),
+      description: yup.string().notRequired().defined(),
+      workspacePhoto: yup.string().notRequired(),
+      accessLink: yup.string().notRequired(),
+      private: yup.bool().notRequired(),
+      users: yup.array().of(yup.string()).notRequired(),
+      usersEmails: yup.array().of(yup.string()).notRequired(),
+      employee: yup
+        .object({
+          name: yup.string().required(),
+          role: yup.string().required(),
+          avatar: yup.string().notRequired(),
+        })
+        .notRequired()
+        .nullable(),
+    })
+    .noUnknown()
+    .required(),
+});
 
-  export type addWorkspaceType = yup.InferType<typeof addWorkspace>;
+export type addWorkspaceType = yup.InferType<typeof addWorkspace>;
