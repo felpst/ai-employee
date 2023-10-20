af3f0ea3c1d58e3140212d557d936c56a358717f
felpst <felpst@users.noreply.github.com>
2023-10-19 21:17:05 -0300

    creation list aiEmployees

--- apps/app/src/app/ai-employee/ai-employee.component.ts
+++ apps/app/src/app/ai-employee/ai-employee.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { IAIEmployee } from '@cognum/interfaces';
+import { EmployeeService } from './ai-employee.service';
 
 @Component({
   selector: 'cognum-ai-employee',
   templateUrl: './ai-employee.component.html',
   styleUrls: ['./ai-employee.component.scss'],
- 
-})
-export class AiEmployeeComponent {
 
+})
+export class AiEmployeeComponent implements OnInit {
+  employees: IAIEmployee[] = [];
+
+  constructor(private employeeService: EmployeeService) { }
+
+  ngOnInit() {
+    this.loadEmployees();
+  }
+
+  loadEmployees() {
+    this.employeeService.list().subscribe(
+      employees => {
+        this.employees = employees;
+      },
+      error => {
+        console.error(error);
+      }
+    );
+  }
 }
