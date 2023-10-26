3e6add5af639560963f9d41dc96e7ce41eb151ce
felpst <felpst@users.noreply.github.com>
2023-10-26 14:57:37 -0300

    feat: new contextual splitter

--- libs/knowledge-base/src/helpers/document.helper.ts
+++ libs/knowledge-base/src/helpers/document.helper.ts
@@ -1,11 +1,7 @@
-import { OpenAI } from '@cognum/llm/openai';
-import { LLMChain } from 'langchain/chains';
 import { Document as LangChainDoc } from 'langchain/document';
-import { ChatPromptTemplate } from 'langchain/prompts';
-import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
 import { Document as MongoDoc } from 'mongodb';
+import ContextualTextSplitter from '../splitters/contextual-splitter';
 
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export interface KnowledgeDocument extends Omit<LangChainDoc, 'metadata'> {
   metadata: KnowledgeMetadata;
 }
@@ -26,14 +22,14 @@ export async function splitDocuments<T extends MongoDoc>(
   docsToSplit: T[],
   chunkSize?: number
 ): Promise<KnowledgeDocument[]> {
-  const splitter = new RecursiveCharacterTextSplitter({
+  const splitter = new ContextualTextSplitter({
     chunkSize: chunkSize || 512,
     chunkOverlap: 0,
-    separators: ['.', '\n'],
+    separators: ['\n', '.'],
     keepSeparator: true,
   });
 
-  const langChainDocsInfo: { title: string; data: LangChainDoc }[] = [];
+  const langChainDocs: LangChainDoc[] = [];
   for (const doc of docsToSplit) {
     const data = new LangChainDoc(<KnowledgeDocument>{
       pageContent: doc.data,
@@ -42,31 +38,15 @@ export async function splitDocuments<T extends MongoDoc>(
         updatedAt: doc.updatedAt?.toISOString(),
       },
     });
-    const title = await generateDocumentTitle(data.pageContent);
 
-    langChainDocsInfo.push({ title, data });
+    langChainDocs.push(data);
   }
 
-  return Promise.all(
-    langChainDocsInfo.map(({ data, title }) =>
-      splitter.splitDocuments([data], { chunkHeader: title })
+  const result = await Promise.all(
+    langChainDocs.map((data) =>
+      splitter.splitBySection([data])
     )
   ).then((result) => result.flat() as KnowledgeDocument[]);
-}
 
-async function generateDocumentTitle(text: string): Promise<string> {
-  const llm = new OpenAI({ temperature: 0 });
-
-  const systemTemplate =
-    'You must provide a name for a document inserted by the human. Use this prompt: "DOCUMENT NAME: [document name]".';
-
-  const chatPrompt = ChatPromptTemplate.fromMessages([
-    ['system', systemTemplate],
-    ['human', text],
-  ]);
-
-  const chain = new LLMChain({ llm, prompt: chatPrompt });
-  const title = (await chain.run({})).trim() + '\n\n---\n\n';
-
-  return title;
+  return result
 }
---
+++ libs/knowledge-base/src/splitters/contextual-splitter.ts
@@ -0,0 +1,156 @@
+import { ChatModel } from '@cognum/llm';
+import { writeFile } from 'fs/promises';
+import { Document } from 'langchain/document';
+import { StructuredOutputParser } from 'langchain/output_parsers';
+import { ChatPromptTemplate, HumanMessagePromptTemplate, SystemMessagePromptTemplate } from 'langchain/prompts';
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { z } from 'zod';
+
+interface DocumentContextStructure {
+  documentName: string
+  sections: Array<{
+    name: string
+    startLine: number
+    endLine: number
+  }>
+}
+
+export default class ContextualTextSplitter extends RecursiveCharacterTextSplitter {
+  async splitBySection(documents: Document[]) {
+    return Promise.all(documents.map(async (document) => {
+      document.pageContent = this.removeDoubleLineBreaks(document.pageContent)
+
+      const { documentName, sections: documentSections } = await this.generateContextualData(document.pageContent)
+      await writeFile('./nameAndSections.json', JSON.stringify({ documentName, documentSections}))
+
+      const splitted: Document[] = []
+
+      for (const section of documentSections) {
+        const { name: sectionName, startLine, endLine } = section
+        const sectionParts: Document[] = []
+        
+        const charsInSection = this.countCharactersInLines(document.pageContent, startLine, endLine)
+        const sectionDocument = new Document({
+          pageContent: this.getSectionContent(document.pageContent, startLine, endLine),
+          metadata: {
+            ...document.metadata,
+            loc: {
+              lines: {
+                from: startLine,
+                to: endLine
+              }
+            }
+          }
+        })
+        
+        if (charsInSection > this.chunkSize) {
+          const slices = await this.splitDocuments([sectionDocument])
+          slices.forEach((slice, index) => {
+            // define line coordinates for slices
+            const sliceStartLine =  index === 0 ? startLine : slices[index - 1].metadata.loc.lines.to + 1
+            const sliceEndLine = index === slices.length -1 ? endLine : sliceStartLine + slice.pageContent.split('\n').length
+            
+            slice.metadata = {
+              ...slice.metadata,
+              loc: {
+                lines: {
+                  from: sliceStartLine,
+                  to: sliceEndLine
+                }
+              }
+            }
+          })
+          sectionParts.push(...slices)
+        } else {
+          sectionParts.push(sectionDocument)
+        }
+        
+        sectionParts.forEach((part, partIndex) => {
+          const header = `${documentName} - ${sectionName}${sectionParts.length > 1 ? ` (Part ${partIndex + 1})` : ''}\n\n---\n\n`
+          part.pageContent = header + part.pageContent
+        })
+
+        splitted.push(...sectionParts)
+      }
+
+      return splitted
+    })).then(arrayOfArray => arrayOfArray.flat())
+  }
+
+  private async generateContextualData(text: string): Promise<DocumentContextStructure> {
+    const model = new ChatModel({ temperature: 0, streaming: false });
+
+    const jsonParser = StructuredOutputParser.fromZodSchema(
+      z.object({
+        documentName: z.string().describe("Name of the document based on its content"),
+        sections: z
+          .array(
+            z.object({
+              name: z.string().describe("Name of the section"),
+              startLine: z.number().describe("The line where the named section begins"),
+              endLine: z.number().optional().describe(`The line where the named section ends`),
+            })
+          )
+          .describe(`Array of the definition of document sections`),
+      })
+    )
+  
+    const prompt = new ChatPromptTemplate({
+      promptMessages: [
+        SystemMessagePromptTemplate.fromTemplate(
+          "Format the output for the documents following the instructions: \n{format_instructions}\n Human input: {inputText}"
+        ),
+        HumanMessagePromptTemplate.fromTemplate("{inputText}"),
+      ],
+      partialVariables: { format_instructions: jsonParser.getFormatInstructions() },
+      inputVariables: ["inputText"],
+    });
+  
+    const input = await prompt.format({ inputText: text })
+  
+  
+    const result = await model.invoke(input)
+    return this.tryParseDocumentStructure(result.content);
+  }
+
+  private tryParseDocumentStructure(rawTextStructure: string): DocumentContextStructure {
+    try {
+      const jsonStart = rawTextStructure.indexOf("{");
+      const jsonEnd = rawTextStructure.lastIndexOf("}");
+      
+      const jsonString = rawTextStructure.substring(jsonStart, jsonEnd + 1)
+    
+      return JSON.parse(jsonString)
+    } catch (error) {
+      throw new Error('It was not possible to parse document structure to JSON.')
+    }
+  }
+
+  private countCharactersInLines(text: string, startLine: number, endLine: number) {
+    const lines = text.split('\n');
+    let characterCount = 0;
+  
+    for (let i = startLine - 1; i < endLine && i < lines.length; i++) {
+      characterCount += lines[i].length;
+      if (i < endLine - 1) {
+        characterCount++;
+      }
+    }
+  
+    return characterCount;
+  }
+
+  private getSectionContent(text: string, startLine: number, endLine: number) {
+    const lines = text.split('\n');
+    return lines.slice(startLine -1, endLine).join('\n')
+  }
+
+  private removeDoubleLineBreaks(text: string): string {
+    const cleanedText = text.replace(/(\r\n|\n|\r){2,}/g, '\n');
+    if (cleanedText.match(/(\r\n|\n|\r){2,}/)) {
+      return this.removeDoubleLineBreaks(cleanedText);
+    }
+  
+    return cleanedText;
+  }
+}
