992205871089e6be13ea538b6857d6517d89aaaa
felpst <felpst@users.noreply.github.com>
2023-11-09 18:14:55 -0300

    feat: create SQL tool and Python tool

---
+++ apps/python-api/python_tool.py
@@ -0,0 +1,25 @@
+from langchain_experimental.tools import PythonREPLTool
+from model import Model
+from langchain_experimental.agents.agent_toolkits import create_python_agent
+from langchain.agents.agent_types import AgentType
+
+
+class PythonTool:
+    def __init__(self):
+        self.model = Model.initialize_model()
+        self._agent_executor = self.agent_executor()
+
+    def agent_executor(self):
+        return create_python_agent(
+            llm=self.model,
+            tool=PythonREPLTool(),
+            verbose=True,
+            agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+            agent_executor_kwargs={"handle_parsing_errors": True},
+        )
+
+    def run(self, string):
+        self._agent_executor.run(string)
+
+
+PythonTool().run("What is the 10th fibonacci number?")
---
+++ apps/python-api/sql_tool.py
@@ -0,0 +1,33 @@
+from model import Model
+from langchain.agents.agent_types import AgentType
+from langchain.agents import create_sql_agent
+from langchain.agents.agent_toolkits import SQLDatabaseToolkit
+from langchain.sql_database import SQLDatabase
+import os
+from dotenv import load_dotenv
+
+dotenv_path = "./.env"
+
+load_dotenv(dotenv_path)
+
+
+class SqlTool:
+    def __init__(self):
+        self.model = Model.initialize_model()
+        self.db = SQLDatabase.from_uri(os.getenv("DATABASE"))
+        self.toolkit = SQLDatabaseToolkit(db=self.db, llm=self.model)
+        self._agent_executor = self.agent_executor()
+
+    def agent_executor(self):
+        return create_sql_agent(
+            llm=self.model,
+            toolkit=self.toolkit,
+            verbose=True,
+            agent_type=AgentType.ZERO_SHOT_REACT_DESCRIPTION,
+        )
+
+    def run(self, string):
+        self._agent_executor.run(string)
+
+
+SqlTool().run("Who are the top 3 best selling artists?")
