2f48c819b7118d37570440256a708fe9372fd14d
felpst <felpst@users.noreply.github.com>
2023-11-14 16:17:11 -0300

    feat: correct SQL tool

--- libs/tools/src/lib/tools/sql.tool.ts
+++ libs/tools/src/lib/tools/sql.tool.ts
@@ -1,25 +1,15 @@
 import axios from 'axios';
 import { DynamicTool } from 'langchain/tools';
 
-type dbInfo = {
-  user: string;
-  password: string;
-}
-
-export class ChatHistoryTool extends DynamicTool {
-  constructor(dbPrimario: dbInfo, dbSecundario?: dbInfo) {
+export class SQLTool extends DynamicTool {
+  constructor() {
     super({
       name: 'Chat History',
       description:
-        'Use this when you need search informations you dont know and possible to find in chat history. Input should be a question.',
+        'Use this when you need search informations you dont know and you dont find in chat history, but possible to find in database. Input should be a question.',
       func: async (input: string) => {
         const response = await axios.post(process.env.SQL_TOOL_URL, { input });
-        if (dbSecundario && response.data === 'Not enough data to search') {
-          const second_response = await axios.post(process.env.SQL_TOOL_URL, {
-            input
-          })
-          return second_response.data;
-        }
+
         return response.data;
       },
     });
