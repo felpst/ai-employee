eb7af1ae52450b46a7f90195bf44ba57feedee20
felpst <felpst@users.noreply.github.com>
2023-11-15 23:11:27 -0300

    add canActivate

--- apps/app/src/app/workspaces/workspaces-routing.module.ts
+++ apps/app/src/app/workspaces/workspaces-routing.module.ts
@@ -51,7 +51,7 @@ const routes: Routes = [
       {
         path: 'settings',
         component: SettingsWorkspaceComponent,
-     
+        canActivate:[WorkspaceAdminGuard],
         children: [
           {
             path: 'general',
