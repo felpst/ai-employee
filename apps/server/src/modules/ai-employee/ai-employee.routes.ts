dfd05d0e7046d5e1c2ef23b05c2b48e4b165a991
felpst <felpst@users.noreply.github.com>
2023-11-16 17:34:30 -0300

    fix: Added updating dependencies and removing unnecessary logic in ia-employees

--- apps/server/src/modules/ai-employee/ai-employee.routes.ts
+++ apps/server/src/modules/ai-employee/ai-employee.routes.ts
@@ -1,27 +1,13 @@
 import express, { Router } from 'express';
-import multer from 'multer';
-import jsonParserMiddleware from '../../middlewares/jsonParserMiddleware';
-import multerConfig from '../../middlewares/multerConfig';
 import { authMiddleware } from '../auth/auth.middleware';
 import employeeController from './ai-employee.controller';
 
 const router: Router = express.Router();
 
-router.post(
-  '/',
-  authMiddleware,
-  // YupValidatorMiddleware(addEmployeeSchema),
-  employeeController.create
-);
+router.post('/', authMiddleware, employeeController.create);
 router.get('/', authMiddleware, employeeController.find);
 router.get('/:id', authMiddleware, employeeController.getById);
-router.put(
-  '/:id',
-  authMiddleware,
-  multer(multerConfig).single('avatar'),
-  jsonParserMiddleware,
-  employeeController.update
-);
+router.put('/:id', authMiddleware, employeeController.update);
 router.delete('/:id', authMiddleware, employeeController.delete);
 
 export default router;
