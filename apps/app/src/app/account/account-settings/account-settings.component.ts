94298f9c0531fff1b5686d99de058f5bf56d3ec7
felpst <felpst@users.noreply.github.com>
2023-11-20 09:46:05 -0300

    feat: Added improvements to the user edit page

--- apps/app/src/app/account/account-settings/account-settings.component.ts
+++ apps/app/src/app/account/account-settings/account-settings.component.ts
@@ -1,14 +1,16 @@
-import { Location } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import {
   FormBuilder,
   Validators
 } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
 import { ActivatedRoute, Router } from '@angular/router';
+import { IUser } from '@cognum/interfaces';
+import { CookieService } from 'ngx-cookie-service';
 import { AuthService } from '../../auth/auth.service';
 import { NotificationsService } from '../../services/notifications/notifications.service';
-import { UploadsService } from '../../services/uploads/uploads.service';
 import { UsersService } from '../../services/users/users.service';
+import { DialogComponent } from '../../shared/dialog/dialog.component';
 import { Step } from '../../shared/stepper/stepper.component';
 @Component({
   selector: 'cognum-account-settings',
@@ -19,42 +21,20 @@ export class AccountSettingsComponent implements OnInit {
   navs: Step[] = [
     { title: 'General', routerLink: './' }
   ]
-  name = '';
-  photo: File | null = null;
   updateForm = this.formBuilder.group({
-    name: [this.name, [Validators.required, Validators.minLength(6)]],
-    photo: [this.photo, []],
+    name: ['', [Validators.required, Validators.minLength(6)]],
   });
-  showUpdateError = false;
-  errors = [];
-  showDeleteConfirmation = false;
-  image = '';
-  selectedImage: string | null = null;
-  workspaceId: string | null = null;
-  defaultImage: string = 'assets/icons/user.svg';
 
   constructor(
-    private location: Location,
     private authService: AuthService,
     private formBuilder: FormBuilder,
-    private router: Router,
     private notificationsService: NotificationsService,
     private usersService: UsersService,
-    private uploadsService: UploadsService,
-    private route: ActivatedRoute
-  ) {
-    this.updateForm.valueChanges.subscribe(() => {
-      this.showUpdateError = false;
-    });
-  }
-
-  openDeleteAccountModal() {
-    this.showDeleteConfirmation = true;
-  }
-
-  onRedirect() {
-    this.location.back();
-  }
+    private route: ActivatedRoute,
+    private dialog: MatDialog,
+    private router: Router,
+    private cookieService: CookieService
+  ) { }
 
   ngOnInit() {
     const resolvedData = this.route.snapshot.data;
@@ -62,28 +42,12 @@ export class AccountSettingsComponent implements OnInit {
       this.updateForm.patchValue({
         name: resolvedData[0].name,
       });
-      this.name = resolvedData[0].name;
-      this.image = resolvedData[0].photo;
     }
   }
 
-  confirmDeleteAccount() {
-    this.usersService.delete(this.user._id).subscribe({
-      next: () => {
-        this.router.navigate(['/auth/register']);
-      },
-    });
-
-    this.showDeleteConfirmation = false;
-  }
-
-  cancelDeleteAccount() {
-    this.showDeleteConfirmation = false;
-  }
-
-  updatePhoto(url: any) {
-    console.log({ url });
+  updatePhoto(url: string) {
     this.user.photo = url;
+    return this.upadteData({ photo: url });
   }
 
   hasInputError(inputName: string, errorName: string) {
@@ -97,25 +61,50 @@ export class AccountSettingsComponent implements OnInit {
   async onSubmit() {
     if (!this.updateForm.valid) return;
     const { name } = this.updateForm.value;
-    const data = { ...this.user };
-    if (!!name && name.length >= 6) {
-      data.name = name;
-    }
-
-    this.usersService.update(this.user._id, { ...data }).subscribe({
-      next: () => {
-        this.notificationsService.show('Successfully changed data!');
-      },
-    });
+    return this.upadteData({ name: name || '' });
   }
 
-  selectedItem: number | null = 1;
+  onDeleteUser() {
+    const dialogData = new DialogComponent({ title: 'Delete', content: 'Are you sure you want to delete your account?', confirmText: 'Yes' });
 
-  selectItem(itemNumber: number): void {
-    this.selectedItem = itemNumber;
+    const dialogRef = this.dialog.open(DialogComponent, {
+      maxWidth: "400px",
+      data: dialogData
+    });
+    dialogRef.afterClosed().subscribe(dialogResult => {
+      if (dialogResult) {
+        this.deleteUser();
+      }
+    });
   }
 
   get user() {
     return this.authService.user;
+  }
+
+  private upadteData(data: Partial<IUser>) {
+    this.usersService.update(this.user._id, { ...this.user, ...data }).subscribe({
+      next: () => {
+        this.notificationsService.show('Successfully changed data!');
+      },
+      error: (error) => {
+        console.log('An error occurred while updating the information', { error });
+        this.notificationsService.show('An error occurred while updating the data, please try again in a moment');
+      }
+    });
+  }
+
+  private deleteUser() {
+    this.usersService.delete(this.user._id).subscribe({
+      next: () => {
+        this.cookieService.delete('token')
+        this.notificationsService.show('Operation carried out successfully!');
+        this.router.navigate(['/']);
+      },
+      error: (error) => {
+        console.log('An error occurred while deleting information', { error });
+        this.notificationsService.show('There was an error deleting your account, please try again in a moment');
+      }
+    });
   }
 }
