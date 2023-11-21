6cb4b368979f489e509515a10311e782d8c6e4ef
felpst <felpst@users.noreply.github.com>
2023-11-21 11:02:17 -0300

    stage environment

---
+++ apps/app/src/environments/environment.stage.ts
@@ -0,0 +1,9 @@
+export const env = {
+  production: false,
+  apis: {
+    core: {
+      url: 'https://staging-server-dot-cognum.uc.r.appspot.com',
+      chatUrl: 'wss://staging-chat-dot-cognum.uc.r.appspot.com/chat',
+    },
+  },
+};
