7487a813a6957581a09c93205d40afcc38a87865
felpst <felpst@users.noreply.github.com>
2023-11-21 16:56:34 -0300

    created tools component

---
+++ apps/app/src/app/workspaces/ai-employees/ai-employee-settings/ai-employee-settings-routing.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AIEmployeeSettingsComponent } from './ai-employee-settings.component';
+import { AIEmployeeGeneralComponent } from './general/general.component';
+import { AIEmployeeToolsComponent } from './tools/tools.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        component: AIEmployeeSettingsComponent,
+        children: [
+            {
+                path: '',
+                data: { nav: { select: 0 } },
+                component: AIEmployeeGeneralComponent,
+            },
+            {
+                path: 'tools',
+                data: { nav: { select: 1 } },
+                component: AIEmployeeToolsComponent
+            },
+            { path: '**', redirectTo: '', pathMatch: 'full' },
+        ],
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule]
+})
+export class AIEmployeeSettingsRoutingModule { }
