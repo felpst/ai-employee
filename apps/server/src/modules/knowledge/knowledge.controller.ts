6e5db9c54d170d7bb443141f4eb30a845ce8d33e
felpst <felpst@users.noreply.github.com>
2023-11-24 12:45:02 -0300

    refactor: manage milvus files by middleware

--- apps/server/src/modules/knowledge/knowledge.controller.ts
+++ apps/server/src/modules/knowledge/knowledge.controller.ts
@@ -1,9 +1,12 @@
 import { IKnowledge } from '@cognum/interfaces';
+import KnowledgeBase, { KnowledgeMetadata } from '@cognum/knowledge-base';
 import { ChatModel } from '@cognum/llm';
 import { Knowledge } from '@cognum/models';
 import { NextFunction, Request, Response } from 'express';
 import { LLMChain } from 'langchain/chains';
+import { Document } from 'langchain/document';
 import { PromptTemplate } from 'langchain/prompts';
+import mongoose from 'mongoose';
 import ModelController from '../../controllers/model.controller';
 
 export class KnowledgeController extends ModelController<typeof Knowledge> {
@@ -117,6 +120,71 @@ export class KnowledgeController extends ModelController<typeof Knowledge> {
       const id: string = req.params.workspaceId;
       const knowledges = await Knowledge.find({ workspace: id });
       res.json(knowledges);
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async createKnowledgeBaseDocument(req: Request, _: Response, next: NextFunction) {
+    try {
+      const bodyArray = Array.isArray(req.body) ? req.body : [req.body];
+
+      const newBody = [];
+      for (const knowledge of bodyArray) {
+        const knowledgeId = new mongoose.mongo.ObjectId();
+        const vectorDb = new KnowledgeBase(knowledge.workspace.toString());
+
+        const doc = new Document<KnowledgeMetadata>({
+          pageContent: knowledge.data,
+          metadata: {
+            ownerDocumentId: knowledgeId.toString(),
+            updatedAt: new Date().toISOString()
+          }
+        });
+        await vectorDb.addDocuments([doc]);
+
+        knowledge['_id'] = knowledgeId;
+        newBody.push(knowledge);
+      }
+
+      req.body = newBody;
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async deleteKnowledgeBaseDocument(req: Request, _: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const { workspace } = await Knowledge.findById(id).select('workspace');
+
+      await new KnowledgeBase(workspace.toString())
+        .deleteDocumentsByOwnerDocumentId(id);
+
+      next();
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  public async updateKnowledgeBaseDocument(req: Request, _: Response, next: NextFunction) {
+    try {
+      const { id } = req.params;
+      const knowledge = req.body;
+      const vectorDb = new KnowledgeBase(knowledge.workspace.toString());
+
+      await vectorDb.deleteDocumentsByOwnerDocumentId(id);
+      const doc = new Document<KnowledgeMetadata>({
+        pageContent: knowledge.data,
+        metadata: {
+          ownerDocumentId: id,
+          updatedAt: new Date().toISOString()
+        }
+      });
+      await vectorDb.addDocuments([doc]);
+
+      next();
     } catch (error) {
       next(error);
     }
