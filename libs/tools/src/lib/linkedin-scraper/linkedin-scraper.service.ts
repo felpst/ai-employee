50f4bc93e43e19ed211ec13138eb3fb6bcba5c44
felpst <felpst@users.noreply.github.com>
2023-11-28 10:00:39 -0300

    feat: implement while

--- libs/tools/src/lib/linkedin-scraper/linkedin-scraper.service.ts
+++ libs/tools/src/lib/linkedin-scraper/linkedin-scraper.service.ts
@@ -1,17 +1,19 @@
 import { Builder, By, Key, WebDriver, until } from 'selenium-webdriver';
-import { Person } from './person.entitie';
+import { structInfos } from './structInfos.utils';
 
 async function getDriver(profession: string): Promise<void> {
     const driver: WebDriver = await new Builder().forBrowser('chrome').build();
+
     await driver.manage().window().maximize();
+
     try {
         await driver.get('https://www.linkedin.com/login');
 
         const userInput = await driver.wait(until.elementLocated(By.id('username')), 5000);
-        await userInput.sendKeys('renato@cognum.ai');
+        await userInput.sendKeys('devrenatorodrigues@gmail.com');
 
         const passwordInput = await driver.findElement(By.id('password'));
-        await passwordInput.sendKeys('soeusei2023', Key.ENTER);
+        await passwordInput.sendKeys('Cs4ever1305@', Key.ENTER);
 
         await driver.wait(until.urlIs('https://www.linkedin.com/feed/'), 100000);
 
@@ -31,46 +33,49 @@ async function getDriver(profession: string): Promise<void> {
         })
 
         const div = await driver.wait(until.elementLocated(By.xpath('//h2/div')), 1500000);
+
         await driver.wait(until.elementTextContains(div, 'About'), 1500000);
+
         await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
         await driver.wait(until.elementLocated(By.className('artdeco-pagination')), 1500000);
-        const buttonNext = await driver.findElements(By.className('artdeco-button'));
+
+        await driver.wait(until.elementLocated(By.className('artdeco-pagination__button')), 1500000);
+
+        const buttonNext = await driver.findElements(By.className('artdeco-pagination__button'));
+
 
 
         //TODO: LOOP TO GET ALL PAGES
-        buttonNext.forEach(async (button) => {
+        for (const button of buttonNext) {
             const buttonName = await button.getText();
             console.log(buttonName);
             if (buttonName === 'Next') {
                 button.click();
+
+
+                await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+                await driver.wait(
+                    until.elementsLocated(By.className('entity-result__content')),
+                    20000,
+                    'Elementos não foram encontrados dentro do tempo limite.'
+                );
+
+
+
+                const profilesList = await driver.findElements(By.className('entity-result__content'));
+                console.log('list', profilesList);
+
+                const promises = structInfos(profilesList);
+
+                const personArray = await Promise.all(promises)
+                console.log(personArray)
             }
-        })
-        await driver.wait(
-            until.elementsLocated(By.className('entity-result__content')),
-            20000,
-            'Elementos não foram encontrados dentro do tempo limite.'
-        );
-
-        const profilesList = await driver.findElements(By.className('app-aware-link'));
-        console.log('list', profilesList);
-
-        const personArray: Person[] = [];
-        profilesList.forEach(async (profile) => {
-            const nameLinkElement = await profile.findElement(By.className('app-aware-link'))
-            const name = (await nameLinkElement.getText()).split('\n')[0];
-            const title = await profile.findElement(By.className('entity-result__primary-subtitle')).getText();
-            const location = await profile.findElement(By.className('entity-result__secondary-subtitle')).getText();
-            const profileLink = await nameLinkElement.getAttribute('href');
-            await Promise.all([name, title, location, profileLink])
-            const person = new Person(name, title, location, profileLink);
-            console.log(person);
-            personArray.push(person);
-        });
-        console.log(personArray);
-
+        }
     } catch (error) {
         console.error(error);
     }
 }
 
-getDriver('Machine Learning Engineer');
+getDriver('Programador em são paulo');
--- libs/tools/src/lib/linkedin-scraper/structInfos.utils.ts
+++ libs/tools/src/lib/linkedin-scraper/structInfos.utils.ts
@@ -1,39 +1,19 @@
-import { IPerson } from "./person.entitie";
+import { By, WebElement } from "selenium-webdriver";
+import { Person } from "./person.entitie";
 
-export function structInfos(persons: IPerson[]): IPerson[] {
-    return persons.map((person) => {
-        return {
-            name: person.name,
-            title: person.title,
-            location: person.location,
-            current: person.current,
-        };
+export function structInfos(persons: WebElement[]): Promise<Person>[] {
+    return persons.map(async (profile) => {
+        try {
+            const nameLinkElement = await profile.findElement(By.className('app-aware-link'))
+            const name = (await nameLinkElement.getText()).split('\n')[0];
+            const title = await profile.findElement(By.className('entity-result__primary-subtitle')).getText();
+            const location = await profile.findElement(By.className('entity-result__secondary-subtitle')).getText();
+            const profileLink = await nameLinkElement.getAttribute('href');
+            const person = new Person(name, title, location, profileLink);
+            return person;
+        } catch (error) {
+            console.error('ocorreu um error', error)
+            return null
+        }
     });
-}
-
-const personOne: IPerson = {
-    name: 'name',
-    title: 'title',
-    location: 'location',
-    current: 'current'
-}
-
-const personTwo: IPerson = {
-    name: 'name2',
-    title: 'title2',
-    location: 'location2',
-    current: 'current2'
-}
-
-const personThree: IPerson = {
-    name: 'name3',
-    title: 'title3',
-    location: 'location3',
-    current: 'current3'
-}
-
-const persons: IPerson[] = [personOne, personTwo, personThree];
-
-console.log(persons);
-
-console.log(structInfos(persons));
+};
