0c3952585b3922e82cead1301e8e11fd147eb409
felpst <felpst@users.noreply.github.com>
2023-11-29 11:34:05 -0300

    feat: create class linkedinScraper and method forProfession

--- libs/tools/src/lib/linkedin-scraper/linkedin-scraper.service.ts
+++ libs/tools/src/lib/linkedin-scraper/linkedin-scraper.service.ts
@@ -1,81 +1,102 @@
 import { Builder, By, Key, WebDriver, until } from 'selenium-webdriver';
+import { Person } from './person.entitie';
 import { structInfos } from './structInfos.utils';
 
-async function getDriver(profession: string): Promise<void> {
-    const driver: WebDriver = await new Builder().forBrowser('chrome').build();
+export class LinkedinScraper {
+    private _username: string;
+    private _password: string;
 
-    await driver.manage().window().maximize();
+    constructor(
+        username: string,
+        password: string
+    ) {
+        this._username = username;
+        this._password = password;
+    }
 
-    try {
-        await driver.get('https://www.linkedin.com/login');
+    async forProfession(profession: string, quantity: number = 10): Promise<Person[]> {
+        const driver: WebDriver = await new Builder().forBrowser('chrome').build();
 
-        const userInput = await driver.wait(until.elementLocated(By.id('username')), 5000);
-        await userInput.sendKeys('devrenatorodrigues@gmail.com');
+        await driver.manage().window().maximize();
 
-        const passwordInput = await driver.findElement(By.id('password'));
-        await passwordInput.sendKeys('Cs4ever1305@', Key.ENTER);
+        try {
+            await driver.get('https://www.linkedin.com/login');
 
-        await driver.wait(until.urlIs('https://www.linkedin.com/feed/'), 100000);
+            const userInput = await driver.wait(until.elementLocated(By.id('username')), 5000);
+            await userInput.sendKeys(this._username);
 
-        const searchInput = await driver.findElement(By.xpath("//input[@placeholder='Search']"));
-        searchInput.clear()
-        await searchInput.sendKeys(profession, Key.ENTER);
+            const passwordInput = await driver.findElement(By.id('password'));
+            await passwordInput.sendKeys(this._password, Key.RETURN);
 
-        await driver.wait(until.urlContains('linkedin.com/search/results'), 50000);
+            await driver.wait(until.urlIs('https://www.linkedin.com/feed/'), 100000);
 
-        const findButtons = await driver.findElements(By.className('search-reusables__filter-pill-button'));
+            const searchInput = await driver.findElement(By.xpath("//input[@placeholder='Search']"));
+            searchInput.clear()
+            await searchInput.sendKeys(profession, Key.RETURN);
 
-        findButtons.forEach(async (button) => {
-            const buttonName = await button.getText();
-            if (buttonName === 'People') {
-                button.click();
+            await driver.wait(until.urlContains('linkedin.com/search/results'), 50000);
+
+            const findButtons = await driver.findElements(By.className('search-reusables__filter-pill-button'));
+
+            findButtons.forEach(async (button) => {
+                const buttonName = await button.getText();
+                if (buttonName === 'People') {
+                    button.click();
+                }
+            })
+
+            const div = await driver.wait(until.elementLocated(By.xpath('//h2/div')), 1500000);
+
+            await driver.wait(until.elementTextContains(div, 'About'), 1500000);
+
+            await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+            await driver.wait(until.elementLocated(By.className('artdeco-pagination')), 1500000);
+
+            await driver.wait(until.elementLocated(By.className('artdeco-pagination__button')), 1500000);
+
+            const persons = [];
+
+            try {
+                while (persons.length < quantity) {
+                    let nextButton = null
+
+                    await driver.wait(
+                        until.elementsLocated(By.className('entity-result__content')),
+                        20000,
+                        'Elements not found within the time limit.'
+                    );
+
+                    const profilesList = await driver.findElements(By.className('entity-result__content'));
+                    const personsInfos = structInfos(profilesList);
+                    const personArray = await Promise.all(personsInfos);
+                    persons.push(...personArray);
+                    console.log(persons);
+
+                    driver.sleep(5000);
+
+                    await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+                    nextButton = await driver.wait(until.elementLocated(By.xpath("//button[contains(., 'Next')]")), 10000);
+                    nextButton = await driver.findElement(By.xpath("//button[contains(., 'Next')]"));
+                    await driver.wait(until.elementIsEnabled(nextButton), 5000);
+                    await nextButton.click();
+
+                    await driver.wait(
+                        until.stalenessOf(profilesList[profilesList.length - 1]),
+                        20000,
+                        'Elements not found within the time limit.'
+                    );
+
+                }
+                return persons;
+            } catch (error) {
+                error.message = 'Not enough profiles found.';
             }
-        })
-
-        const div = await driver.wait(until.elementLocated(By.xpath('//h2/div')), 1500000);
-
-        await driver.wait(until.elementTextContains(div, 'About'), 1500000);
-
-        await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-
-        await driver.wait(until.elementLocated(By.className('artdeco-pagination')), 1500000);
-
-        await driver.wait(until.elementLocated(By.className('artdeco-pagination__button')), 1500000);
-
-        const buttonNext = await driver.findElements(By.className('artdeco-pagination__button'));
-
-
-
-        //TODO: LOOP TO GET ALL PAGES
-        for (const button of buttonNext) {
-            const buttonName = await button.getText();
-            console.log(buttonName);
-            if (buttonName === 'Next') {
-                button.click();
-
-
-                await driver.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-
-                await driver.wait(
-                    until.elementsLocated(By.className('entity-result__content')),
-                    20000,
-                    'Elementos não foram encontrados dentro do tempo limite.'
-                );
-
-
-
-                const profilesList = await driver.findElements(By.className('entity-result__content'));
-                console.log('list', profilesList);
-
-                const promises = structInfos(profilesList);
-
-                const personArray = await Promise.all(promises)
-                console.log(personArray)
-            }
+        } catch (error) {
+            error.message = 'Not enough profiles found.';
         }
-    } catch (error) {
-        console.error(error);
+        finally {
+            await driver.quit();
+        }
     }
 }
-
-getDriver('Programador em são paulo');
--- libs/tools/src/lib/linkedin-scraper/person.entitie.ts
+++ libs/tools/src/lib/linkedin-scraper/person.entitie.ts
@@ -12,22 +12,16 @@ export class Person implements IPerson {
     title: string;
     location: string;
     profileLink?: string;
-    current?: string;
     constructor(
         name: string,
         title: string,
         location: string,
         profileLink?: string,
-        current?: string
     ) {
         this.name = name;
         this.title = title;
         this.location = location;
         this.profileLink = profileLink;
-        this.current = current;
-    }
+    };
 
-}
-
-const person = new Person('name', 'title', 'location', 'current');
-console.log(person);
+};
