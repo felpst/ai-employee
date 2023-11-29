a586d67200e6cf8383161e58017cd50add034b70
felpst <felpst@users.noreply.github.com>
2023-11-29 14:36:59 -0300

    tools ajustments

--- apps/app/src/app/workspaces/ai-employees/ai-employee-settings/tools/tool-settings/database/tool-settings-database.component.ts
+++ apps/app/src/app/workspaces/ai-employees/ai-employee-settings/tools/tool-settings/database/tool-settings-database.component.ts
@@ -5,35 +5,35 @@ import { ToolsHelper } from "@cognum/helpers";
 import { IToolSettings } from "@cognum/interfaces";
 
 @Component({
-    selector: 'cognum-ai-tool-settings-database',
-    templateUrl: './tool-settings-database.component.html',
-    styleUrls: ['./tool-settings-database.component.scss'],
+  selector: 'cognum-ai-tool-settings-database',
+  templateUrl: './tool-settings-database.component.html',
+  styleUrls: ['./tool-settings-database.component.scss'],
 })
 export class AIToolSettingsDatabaseComponent {
-    formGroup: FormGroup
+  formGroup: FormGroup
 
-    constructor(
-        @Inject(MAT_DIALOG_DATA) public data: { tool: IToolSettings; },
-        private dialogRef: MatDialogRef<AIToolSettingsDatabaseComponent>,
-    ) {
-        this.formGroup = new FormGroup({
-            database: new FormControl(this.data.tool.options?.database, [Validators.required]),
-            db_name: new FormControl(this.data.tool.options?.db_name, [Validators.required]),
-            host: new FormControl(this.data.tool.options?.host, [Validators.required]),
-            db_port: new FormControl(this.data.tool.options?.db_port, [Validators.required]),
-            auth: new FormGroup({
-                username: new FormControl(this.data.tool.options?.auth?.username, [Validators.required]),
-                password: new FormControl(this.data.tool.options?.auth?.password, [Validators.required]),
-            })
-        });
-    }
+  constructor(
+    @Inject(MAT_DIALOG_DATA) public data: { tool: IToolSettings; },
+    private dialogRef: MatDialogRef<AIToolSettingsDatabaseComponent>,
+  ) {
+    this.formGroup = new FormGroup({
+      database: new FormControl(this.data.tool.options?.database, [Validators.required]),
+      name: new FormControl(this.data.tool.options?.name, [Validators.required]),
+      host: new FormControl(this.data.tool.options?.host, [Validators.required]),
+      port: new FormControl(this.data.tool.options?.port, [Validators.required]),
+      auth: new FormGroup({
+        user: new FormControl(this.data.tool.options?.auth?.user, [Validators.required]),
+        pass: new FormControl(this.data.tool.options?.auth?.pass, [Validators.required]),
+      })
+    });
+  }
 
-    get tool() {
-        return ToolsHelper.get(this.data.tool.id);
-    }
+  get tool() {
+    return ToolsHelper.get(this.data.tool.id);
+  }
 
-    onSubmit() {
-        const data = this.formGroup.value;
-        this.dialogRef.close(data);
-    }
+  onSubmit() {
+    const data = this.formGroup.value;
+    this.dialogRef.close(data);
+  }
 }
