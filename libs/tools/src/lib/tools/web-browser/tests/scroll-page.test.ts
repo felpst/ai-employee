89437929cc82688946a29d5bfb050ef60cb02d41
felpst <felpst@users.noreply.github.com>
2023-12-13 17:39:49 -0300

    improve: Added improvement to the scroll tool to enable scrolling to a specific element on the page

--- libs/tools/src/lib/tools/web-browser/tests/scroll-page.test.ts
+++ libs/tools/src/lib/tools/web-browser/tests/scroll-page.test.ts
@@ -15,7 +15,15 @@ describe('Scroll Page tool test', () => {
   it('Scroll page to location', async () => {
     const result = await service.scrollPage(4235);
     expect(result).toBe(true);
-  });
+  })
+
+  it('Scroll page to element in page', async () => {
+    const result = await service.scrollPage(null, {
+      selectorType: 'id',
+      fieldSelector: 'bstn-launcher'
+    });
+    expect(result).toBe(true);
+  })
 
   afterAll(async () => {
     await webBrowser.driver.close();
--- libs/tools/src/lib/tools/web-browser/web-browser-scroll-page.tool.ts
+++ libs/tools/src/lib/tools/web-browser/web-browser-scroll-page.tool.ts
@@ -1,6 +1,6 @@
 import { DynamicStructuredTool } from 'langchain/tools';
 import { z } from 'zod';
-import { WebBrowserService } from './web-browser.service';
+import { ElementSelector, IElementFindOptions, WebBrowserService } from './web-browser.service';
 import { WebBrowserToolSettings } from './web-browser.toolkit';
 
 export class WebBrowserScrollPageTool extends DynamicStructuredTool {
@@ -10,13 +10,16 @@ export class WebBrowserScrollPageTool extends DynamicStructuredTool {
       metadata: { id: "web-browser", tool: 'scrollPage' },
       description: 'Use this tool to scroll page vertically.',
       schema: z.object({
-        location: z.number().describe("the location where the page will be scrolled")
+        location: z.number().optional().describe("the location where the page will be scrolled"),
+        fieldSelector: z.string().optional().describe("the selector of the html field element"),
+        selectorType: z.nativeEnum(ElementSelector).optional().describe("type of the selector"),
+        findTimeout: z.number().optional().default(10000).describe("timeout to find the input element in ms")
       }),
-      func: async ({ location }: { location: number; }) => {
+      func: async ({ location, ...params }: IElementFindOptions & { location: number; }) => {
         try {
           const browserService = new WebBrowserService(settings.webBrowser);
 
-          const success = await browserService.scrollPage(location);
+          const success = await browserService.scrollPage(location, params);
           if (!success) throw new Error(`Location scroll unsuccessful`);
 
           return `Location scroll to ${location} was successfully done`;
