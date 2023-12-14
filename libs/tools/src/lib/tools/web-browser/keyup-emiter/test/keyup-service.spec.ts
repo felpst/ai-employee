a9d3a1f9583e7607f931fc6eb6220869f40474dc
felpst <felpst@users.noreply.github.com>
2023-12-14 08:58:51 -0300

    test: add combination

--- libs/tools/src/lib/tools/web-browser/keyup-emiter/test/keyup-service.spec.ts
+++ libs/tools/src/lib/tools/web-browser/keyup-emiter/test/keyup-service.spec.ts
@@ -3,8 +3,14 @@ import { KeyUpService } from '../keyup-emiter.service';
 describe('Key up service', () => {
   it('press', async () => {
     const service = new KeyUpService();
-    const result = await service.press('Enter');
-    expect(result).toBe('Key Enter pressed');
+    const result = await service.press('@10');
+    expect(result).toBe('Key @10 pressed');
+  })
+
+  it('press combination', async () => {
+    const service = new KeyUpService();
+    const result = await service.press('@66', ['@65']);
+    expect(result).toBe('Keys @65,@66 pressed');
   })
 
   it('typeMessage', async () => {
