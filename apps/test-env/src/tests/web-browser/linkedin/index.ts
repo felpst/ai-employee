1aaa2f63839ab3791b231a0edd237a4c21a0ea0c
felpst <felpst@users.noreply.github.com>
2023-12-19 09:19:20 -0300

    test: linkedin leads extract test with web browser service

--- apps/test-env/src/tests/web-browser/linkedin/index.ts
+++ apps/test-env/src/tests/web-browser/linkedin/index.ts
@@ -25,34 +25,42 @@ export class LinkedinTest {
     const passwordContext = 'Password field';
     const passwordElement = await this.service.findElementByContext(passwordContext);
 
-    const inputPassword = await this.service.inputText(process.env.LINKEDIN_PASSWORD, {
+    await this.service.inputText(process.env.LINKEDIN_PASSWORD, {
       selectorType: passwordElement.selectorType,
       elementSelector: passwordElement.selector,
     });
 
     const buttonContext = 'Login button';
     const buttonElement = await this.service.findElementByContext(buttonContext);
-    const buttonClick = await this.service.click({
+    await this.service.click({
       selectorType: buttonElement.selectorType,
       elementSelector: buttonElement.selector,
     });
 
     const searchContext = 'Search field';
     const searchElement = await this.service.findElementByContext(searchContext);
-    const inputSearch = await this.service.inputText('Web Developer in Brazil', {
+    await this.service.inputText('Web Developer in Brazil', {
       selectorType: searchElement.selectorType,
       elementSelector: searchElement.selector,
     });
 
-    // TODO enter / clicar no botão
-    return;
+    await this.service.keyupEmiter('\uE007');
 
-    const peopleButtonContext = 'People button filter';
+    const peopleButtonContext = 'filter people button';
     const peopleButtonElement = await this.service.findElementByContext(peopleButtonContext);
-    const peopleButtonClick = await this.service.click({
+    await this.service.click({
       selectorType: peopleButtonElement.selectorType,
       elementSelector: peopleButtonElement.selector,
     });
+
+    const extractDataContext = 'Extract data of people';
+    const extractDataElement = await this.service.findElementByContext(extractDataContext);
+    const result = await this.service.extractData({
+      selectorType: extractDataElement.selectorType,
+      elementSelector: extractDataElement.selector,
+    });
+    console.log('result', result);
+
 
     const nextButtonContext = 'next Button in page';
     const nextButtonElement = await this.service.findElementByContext(nextButtonContext);
