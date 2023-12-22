79bb2c8d8e177712d8f78ec4afe0b8a43deeae12
felpst <felpst@users.noreply.github.com>
2023-12-22 11:54:44 -0300

    feat: prompt adjust and axios timeout

--- libs/tools/src/lib/tools/csv-assistant/csv-assistant.tool.ts
+++ libs/tools/src/lib/tools/csv-assistant/csv-assistant.tool.ts
@@ -15,7 +15,7 @@ export class CSVAssistant extends DynamicTool {
         'Use to get the results of an analysis of one or many spreadsheet files saved. Input should be a question.',
       func: async (input: string) => {
         try {
-          const TOOL_INSTRUCTIONS = 'You are data scientist. Before you start, you need to load and prepare data frames, understand what data is in them and what it means.';
+          const TOOL_INSTRUCTIONS = 'You are data scientist. Before you start, you need to load and prepare data frames, understand what data is in them and what it means. In case you need a lib that is not in the environment, use the lib subprocess to install them.';
 
           const knowledgeUrls =
             (await Knowledge.find({ workspace: settings.workspaceId })
@@ -23,12 +23,12 @@ export class CSVAssistant extends DynamicTool {
               .lean())
               .map(({ contentUrl }) => contentUrl);
 
-          const csvFileUrls = knowledgeUrls.filter(url => url.endsWith('.csv'));
+          const csvFileUrls = knowledgeUrls.filter(url => url?.endsWith('.csv'));
           const { data } = await axios.post(`${process.env.PYTHON_SERVICE_URL}/python`, {
             input_text: input,
             instructions: TOOL_INSTRUCTIONS,
             file_urls: csvFileUrls
-          });
+          }, { timeout: 300000 });
           return data.result;
         } catch (error) {
           console.log(error);
