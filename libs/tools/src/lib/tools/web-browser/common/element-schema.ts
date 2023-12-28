b5c0c872d03066a1749790cec029751f663434f3
felpst <felpst@users.noreply.github.com>
2023-12-28 13:47:08 -0300

    feat: export element selectors

--- libs/tools/src/lib/tools/web-browser/common/element-schema.ts
+++ libs/tools/src/lib/tools/web-browser/common/element-schema.ts
@@ -1,19 +1,18 @@
 import { z } from 'zod';
 
-const validSelectors: Array<'id' | 'className' | 'name' | 'xpath' | 'css' | 'js' | 'linkText' | 'partialLinkText'> =
-  ['id', 'className', 'name', 'xpath', 'css', 'js', 'linkText', 'partialLinkText'];
+export const ELEMENT_SELECTORS = ['id', 'name', 'xpath', 'css', 'js', 'linkText', 'partialLinkText'] as const;
 
 export const elementSchema = z.object({
   elementSelector: z.string().describe("the selector of the html element."),
-  selectorType: z.enum(validSelectors as any).describe("type of the selector."),
+  selectorType: z.enum(ELEMENT_SELECTORS).describe("type of the selector."),
   findTimeout: z.number().optional().default(10000).describe("timeout to find the element in ms.")
 });
 
 type SchemaType = z.infer<typeof elementSchema>;
 
-export type ElementSelector = typeof validSelectors[number];
+export type ElementSelector = typeof ELEMENT_SELECTORS[number];
 
 export interface IElementFindOptions extends SchemaType {
   elementSelector: NonNullable<SchemaType['elementSelector']>;
-  selectorType: NonNullable<ElementSelector>;
+  selectorType: NonNullable<SchemaType['selectorType']>;
 }
